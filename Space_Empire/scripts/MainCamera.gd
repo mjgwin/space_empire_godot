dfba80993a21ea30d745dabb5107e827744a2bb9
mjgwin <micahjgwin@gmail.com>
2022-11-10 23:44:30 -0600

    improved camera movement and resolution

--- Space_Empire/scripts/MainCamera.gd
+++ Space_Empire/scripts/MainCamera.gd
@@ -1,25 +1,41 @@
 extends Camera2D
 
-var cam_speed = 300
-var zoom_speed = 0.05
+var cam_speed = 350
+var zoom_speed = 3
 var speed_zoom_adj = 8
+
+#example of interpolate camera towards position of mouse when it strays from center
+#var mouse_offset = (get_viewport().get_mouse_position() - get_viewport().size / 2)
+#$Camera2D.position = lerp(Vector2(), mouse_offset.normalized() * 500, mouse_offset.length() / 1000)
 
 func _ready():
 	self.current = true
 	
 func _process(delta):
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_key_pressed(KEY_A):
 		self.global_position.x -= cam_speed * delta * zoom.x
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_key_pressed(KEY_D):
 		self.global_position.x += cam_speed * delta * zoom.x
-	if Input.is_action_pressed("ui_up"):
-		self.global_position.y -= cam_speed * delta * zoom.y
-	if Input.is_action_pressed("ui_down"):
-		self.global_position.y += cam_speed * delta * zoom.y
 	if Input.is_key_pressed(KEY_W):
-		self.zoom.x -= zoom_speed
-		self.zoom.y -= zoom_speed
-		
+		self.global_position.y -= cam_speed * delta * zoom.y
 	if Input.is_key_pressed(KEY_S):
-		self.zoom.x += zoom_speed
-		self.zoom.y += zoom_speed
+		self.global_position.y += cam_speed * delta * zoom.y
+		
+	if Input.is_key_pressed(KEY_EQUAL):
+		self.zoom.x -= zoom_speed * delta
+		self.zoom.y -= zoom_speed * delta
+		
+	if Input.is_key_pressed(KEY_MINUS):
+		self.zoom.x += zoom_speed * delta
+		self.zoom.y += zoom_speed * delta
+		
+#for selecting planet by clicking
+#https://godotengine.org/qa/22606/how-ive-handled-area2d-mouse-click-not-question
+		
+#Example of zoom with mouse wheel action
+#if Input.is_action_just_released("MWU"):
+	#self.zoom.x -= zoom_speed * delta
+	#self.zoom.y -= zoom_speed * delta
+#if Input.is_action_just_released("MWD"):
+	#self.zoom.x += zoom_speed * delta
+	#self.zoom.y += zoom_speed * delta
